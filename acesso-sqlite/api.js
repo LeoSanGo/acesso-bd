28e57659900cc3fe558d38d92ca99bff8f46386e
leosango <souoleo@gmail.com>
2021-11-30 23:55:56 -0300

    implementação metodos da tabela produtos

--- acesso-sqlite/api.js
+++ acesso-sqlite/api.js
@@ -1,4 +1,5 @@
-const categories = require('./categories');
+const categories = require('./categories')('./banco.sqlite3')
+const products = require('./products')('./banco.sqlite3')
 
 const test = async() => {
  /*  await categories.create([1, 'opa 1']);
@@ -7,10 +8,13 @@ const test = async() => {
   await categories.create([4, 'opa 4']);*/
   //await categories.remove([1]);
   //await categories.update(8, ['Categoria atualizada']); 
-  console.log(await categories.findAll());
+  /* console.log(await categories.findAll());
   console.log('cp: 0', await categories.findAllPaginated({ pageSize: 2, currentPage: 0 }));
   console.log('cp: 1', await categories.findAllPaginated({ pageSize: 2, currentPage: 1 }));
-  console.log('cp: 2', await categories.findAllPaginated({ pageSize: 2, currentPage: 2 }));
+  console.log('cp: 2', await categories.findAllPaginated({ pageSize: 2, currentPage: 2 })); */
+
+  // await products.create([1, 'test', 90])
+  await products.update(1, ['new prod', 89]) 
 }
 
 test();
--- acesso-sqlite/products.js
+++ acesso-sqlite/products.js
@@ -3,27 +3,27 @@ const init = database => {
 
   const create = async(data) => {
     const dbConn = await db.init(database);
-    await db.queryWithParams(dbConn, `insert into categories (id, category) values(?, ?)`, data);
+    await db.queryWithParams(dbConn, `insert into products (id, product, price) values(?, ?, ?)`, data);
   }
 
   const findAll = async() => {
     const dbConn = await db.init(database);
-    return await db.query(dbConn, `select * from categories`);
+    return await db.query(dbConn, `select * from products`);
   }
 
   const remove = async(id) => {
     const dbConn = await db.init(database);
-    await db.queryWithParams(dbConn, `delete from categories where id = ?`, [id]);
+    await db.queryWithParams(dbConn, `delete from products where id = ?`, [id]);
   }
 
   const update = async(id, data) => {
     const dbConn = await db.init(database);
-    await db.queryWithParams(dbConn, `update categories set category=? where id=?`, [...data, id]);
+    await db.queryWithParams(dbConn, `update products set product=?, price=? where id=?`, [...data, id]);
   }
 
   const findAllPaginated = async({ pageSize = 1, currentPage = 0 }) => {
     const dbConn = await db.init(database);
-    const records = await db.query(dbConn, `select * from categories limit ${currentPage*pageSize}, ${pageSize+1}`);
+    const records = await db.query(dbConn, `select * from products limit ${currentPage*pageSize}, ${pageSize+1}`);
     if (records.length > pageSize) {
       records.pop();
     }
