55bcc5c7045917f7f03d3fead91aae4345a6af71
leosango <souoleo@gmail.com>
2022-01-04 23:57:44 -0300

    Mudanças na abstração da comunicação com DB

--- acesso-sqlite/db.js
+++ acesso-sqlite/db.js
@@ -1,14 +1,29 @@
 const sqlite = require('sqlite3').verbose();
 
-const init = databaseFile => new Promise((resolve, reject) => {
-    const db = new sqlite.Database(databaseFile, (err) => {
-      if(err) {
-        reject(err)
-      } else {
-        resolve(db)
-      }
+const openDatabase = databaseFile => new Promise((resolve, reject) => {
+  const db = new sqlite.Database(databaseFile, (err) => {
+    if(err) {
+      reject(err)
+    } else {
+      resolve(db)
+    }
   });
 });
+
+const init = async (databaseFile) => {
+  const db = await openDatabase(databaseFile) 
+
+   const exists = await query(db, `SELECT name from sqlite_master where type = 'table' and name = 'categories'`)
+
+  await run(db, `
+    CREATE TABLE categories (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      category TEXT
+    );
+  `) 
+
+  return db
+}
 
 const queryWithParams = (db, query, values) => new Promise((resolve, reject) => {
   db.run(query, values, err => {
